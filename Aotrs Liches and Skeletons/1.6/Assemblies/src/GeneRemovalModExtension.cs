03c554dfc331dcc15406b23b6f6bf84b12ff4d22
AotrsCommander <33864417+AotrsCommander@users.noreply.github.com>
2025-09-25 21:25:00 +0100

    Add files via upload

---
+++ Aotrs Liches and Skeletons/1.6/Assemblies/src/GeneRemovalModExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LichGenePatch
+{
+    public class GeneRemovalModExtension : DefModExtension
+    {
+        public GeneDef keyGene;
+        public List<GeneDef> genesToRemove;
+
+        public bool IsGeneDisallowed(GeneDef Gene)
+        {
+            return genesToRemove.Contains(Gene);
+        }
+    }
+}
