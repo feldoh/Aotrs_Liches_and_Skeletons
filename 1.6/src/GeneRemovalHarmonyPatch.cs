87eefda9201a918a8aa219cd9518adb737eff089
Dexter Lowe <feldoh10@gmail.com>
2025-09-27 00:21:50 +0100

    Reorg

---
+++ 1.6/src/GeneRemovalHarmonyPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace LichGenePatch;
+
+[StaticConstructorOnStartup]
+public static class GeneRemovalHarmonyPatch
+{
+    static GeneRemovalHarmonyPatch()
+    {
+        var harmony = new Harmony("com.yourname.generemoval");
+        harmony.PatchAll();
+    }
+}
---
+++ 1.6/src/GeneRemovalModExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace LichGenePatch;
+
+public class GeneRemovalModExtension : DefModExtension
+{
+    public GeneDef keyGene;
+    public List<GeneDef> genesToRemove;
+
+    public bool IsGeneDisallowed(GeneDef Gene)
+    {
+        return genesToRemove.Contains(Gene);
+    }
+}
